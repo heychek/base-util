91058b082aeee7146056e6590014e3cd8d40704e
hckisagoodboy <1459526461@qq.com>
2018-11-13 15:25:46 +0800

    添加 list 长度判断的工具方法

--- src/main/java/com/github/hckisagoodboy/base/util/common/base/ListUtils.java
+++ src/main/java/com/github/hckisagoodboy/base/util/common/base/ListUtils.java
@@ -18,4 +18,22 @@ public class ListUtils {
   public static boolean isEmpty(List list) {
     return ObjUtils.isNull(list) || list.isEmpty();
   }
+
+  /**
+   * <p>判断传入的所有列表长度是否相等</p>
+   *
+   * @param lists 所有传入的 {@code List} 对象参数
+   * @return 返回值: 需要注意的是, 如果传入的都为 {@code null} 值, 也会返回 {@code true}
+   */
+  public static boolean isLengthsSame(List... lists) {
+    int standard = lists[0] == null ? -1 : lists[0].size();
+    for (List list : lists) {
+      boolean isNullInvalid = (list == null && standard != -1);
+      boolean isLengthInvalid = (list != null && list.size() != standard);
+      if (isNullInvalid || isLengthInvalid) {
+        return false;
+      }
+    }
+    return true;
+  }
 }
