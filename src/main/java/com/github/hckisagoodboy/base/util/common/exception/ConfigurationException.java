6cecda1be1c1e0db0a0933bb5155e57d4a73729a
hckisagoodboy <1459526461@qq.com>
2018-11-13 15:30:59 +0800

    添加配置错误异常类

---
+++ src/main/java/com/github/hckisagoodboy/base/util/common/exception/ConfigurationException.java
@@ -0,0 +1,11 @@
+package com.github.hckisagoodboy.base.util.common.exception;
+
+/**
+ * @author hck
+ * 2018/11/13 3:29 PM
+ */
+public class ConfigurationException extends RuntimeException {
+  public ConfigurationException(String message) {
+    super(message);
+  }
+}
