f90f0f851e8ab5c7d7be9e7bea64ef594a404ef6
hck <kevin_hck@foxmail.com>
2019-03-06 10:54:01 +0800

    add method to JsonParseUtils

--- src/main/java/com/github/codinghck/base/util/common/base/json/JsonParseUtils.java
+++ src/main/java/com/github/codinghck/base/util/common/base/json/JsonParseUtils.java
@@ -7,19 +7,37 @@ import com.alibaba.fastjson.JSONObject;
  * @author hck
  * 2018/11/3 11:45 PM
  */
-@SuppressWarnings("unused")
+@SuppressWarnings({"unused", "WeakerAccess"})
 public class JsonParseUtils {
 
   private JsonParseUtils() {}
 
   /**
    * <p>将一个 <code>T</code> 类型对象转换为 <code>JSONObject</code> 对象</p>
+   *
    * @param obj 需要被转换的原始对象
    * @param <T> 该原始对象的泛型
    * @return 转换后的 <code>JSONObject</code> 对象
    */
-  public static <T> JSONObject objToJsonObj(T obj) {
-    String jsonStr = JSON.toJSONString(obj);
+  public static <T> JSONObject obj2JsonObj(T obj) {
+    String jsonStr = obj2JsonStr(obj);
     return JSONObject.parseObject(jsonStr);
+  }
+
+  /**
+   * <p>将一个 <code>T</code> 类型对象转换为 json 字符串</p>
+   *
+   * @param obj 需要被转换的原始对象
+   * @param <T> 该原始对象的泛型
+   * @return 转换后的 json 字符串
+   */
+  public static <T> String obj2JsonStr(T obj) {
+    String toStr = obj.toString();
+    try {
+      JSONObject.parseObject(toStr);
+      return toStr;
+    } catch (Exception e) {
+      return JSON.toJSONString(obj);
+    }
   }
 }
