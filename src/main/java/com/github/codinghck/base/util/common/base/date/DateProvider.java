c848d3c8a88a9f2fb0ec5dadae0f1d15f58569da
hck <kevin_hck@foxmail.com>
2019-03-11 10:47:43 +0800

    add date provider method

--- src/main/java/com/github/codinghck/base/util/common/base/date/DateProvider.java
+++ src/main/java/com/github/codinghck/base/util/common/base/date/DateProvider.java
@@ -2,6 +2,7 @@ package com.github.codinghck.base.util.common.base.date;
 
 import com.github.codinghck.base.util.common.base.str.StrConst;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 /**
@@ -38,5 +39,58 @@ public class DateProvider {
    */
   public static long currentTimeSeconds() {
     return System.currentTimeMillis() / 1000;
+  }
+
+  /**
+   * <p>获取上一个工作日</p>
+   *
+   * @return 上一个工作日的日期
+   */
+  public static Date preWeekDay() {
+    Date today = new Date();
+    WeekDay weekDay = DateJudgeUtils.dayForWeek(today);
+    int preIdx = weekDay == WeekDay.MONDAY ? -3 : -1;
+    return getByDay(today, preIdx);
+  }
+
+  /**
+   * <p>获取昨天日期</p>
+   *
+   * @return 日期对象
+   */
+  public static Date preDay() {
+    return getByToday(-1);
+  }
+
+  /**
+   * <p>获取明天日期</p>
+   *
+   * @return 日期对象
+   */
+  public static Date nextDay() {
+    return getByToday(1);
+  }
+
+  /**
+   * <p>根据今日的日期获取日期</p>
+   *
+   * @param idx -1 表示获取前一天日期, -2 表示获取前两天, 1 表示获取后一天, 2 表示后两天, 以此类推
+   * @return 日期对象
+   */
+  public static Date getByToday(int idx) {
+    return getByDay(new Date(), idx);
+  }
+
+  /**
+   * <p>根据基准日期获取日期</p>
+   *
+   * @param idx -1 表示获取前一天日期, -2 表示获取前两天, 1 表示获取后一天, 2 表示后两天, 以此类推
+   * @return 日期对象
+   */
+  public static Date getByDay(Date date, int idx) {
+    Calendar calendar = Calendar.getInstance();
+    calendar.setTime(date);
+    calendar.add(Calendar.DAY_OF_MONTH, idx);
+    return calendar.getTime();
   }
 }
